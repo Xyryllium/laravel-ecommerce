01697f36a767f86f18f5aa126c8d8b7ee35028c8
Xy <xyaranzaaa@gmail.com>
2020-10-07 09:35:37 +0800

    Added Voyager
    Added Voyager
    Change Menus to dynamic

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -20,25 +20,13 @@
         <header class="with-background">
             <div class="top-nav container">
                 <div class="logo">Laravel Ecommerce</div>
-                <ul>
-                    <li><a href="{{route('shop.index')}}">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li>
-                        <a href="{{ route('cart.index') }}">Cart 
-                            <span class="cart-count">
-                                @if(Cart::instance('default')->count() > 0)
-                                    <span>{{ Cart::instance('default')->count() }}</span>
-                            </span>
-                        @endif
-                        </a>
-                    </li>
-                </ul>
+                {{ menu('main', 'partials.menus.main')}}
             </div> <!-- end top-nav -->
             <div class="hero container">
                 <div class="hero-copy">
                     <h1>Laravel Ecommerce Demo</h1>
                     <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
+
                     <div class="hero-buttons">
                         <a href="#" class="button button-white">Blog Post</a>
                         <a href="#" class="button button-white">GitHub</a>
